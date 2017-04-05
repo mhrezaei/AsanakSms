b8a80479be56dec9e527755c8af7c4c3776a4cd0
Mohammad Hadi Rezaei <mr.mhrezaei@gmail.com>
2017-04-05 16:17:54 +0430

    Update asanak-sms.php

--- src/Config/asanak-sms.php
+++ src/Config/asanak-sms.php
@@ -2,9 +2,9 @@
 
 return [
 
-    'username' => 'ehda88190180',
-    'password' => 'ehda!centerAsanak88190',
-    'source'   => '02188190180',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'source'   => 'your_source_number',
     'url' => 'http://panel.asanak.ir/webservice/v1rest/sendsms',
 
 ];
